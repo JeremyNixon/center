6db18898b0b845d73ba26af0f445b7f2162925d6
jeremynixon <jnixon2@gmail.com>
2014-08-18 00:29:21 -0400

    debug

--- app/database/migrations/2014_08_18_040008_quiz_table.php
+++ app/database/migrations/2014_08_18_040008_quiz_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Quiz extends Migration {
+class QuizTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -13,7 +13,7 @@ class Quiz extends Migration {
 	public function up()
 	{
 		//
-		Schema::create('quizzzes', function($table){
+		Schema::create('quizzes', function($table){
 			$table->increments('id');
 			$table->integer('q1');
 			$table->integer('q2');
