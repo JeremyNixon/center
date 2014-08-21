61fea7b7c15f1bd3eafa30d760467d4781fc2e19
jeremynixon <jnixon2@gmail.com>
2014-08-20 21:53:00 -0400

    turn of production debugging

--- app/views/behavior/landing.blade.php
+++ app/views/behavior/landing.blade.php
@@ -8,17 +8,17 @@
 </div>
 <div class="home-content-container">
 	<div class="home-left">
-		<a href="/behavior/review">Book Reviews</a><br>
+		<a class="home-left-title" href="/behavior/review">Book Reviews</a><br>
 		<div class="home-content-text">
 		Check out reviews of the Behavioral Economics Literature!
 	</div></div>
 	<div class="home-left">
-		<a href="/behavior/list">Reading List</a><br>
+		<a class="home-left-title" href="/behavior/list">Reading List</a><br>
 		<div class="home-content-text">
 		Check out the list of fascinating Behavioral Economics Literature!
 	</div></div>
 	<div class="home-left">
-		<a href="/behavior/forum">Forum</a><br>
+		<a class="home-left-title" href="/behavior/forum">Forum</a><br>
 		<div class="home-content-text">
 		Be part of a vibrant Behavioral Economics Discussion!
 	</div></div>
