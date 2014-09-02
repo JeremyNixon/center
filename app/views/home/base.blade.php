96e09d983006f8b22e2d40254e687a3ca37d6822
jeremynixon <jnixon2@gmail.com>
2014-09-02 00:39:29 -0400

    timestamp link debut

--- app/views/home/base.blade.php
+++ app/views/home/base.blade.php
@@ -18,8 +18,8 @@ date_default_timezone_set ('america/new_york');
 		<ul id="navlist">
 			<li><a href="/">Home</a><li>
 			<li><a href="/home/reading-list">Reading List</a><li>
-\			<li><a href="/quiz">Philosophy Quiz</a><li>
-			<li><a href="project-four.jeremynixon.me">Timestamp</a><li>
+			<li><a href="/quiz">Philosophy Quiz</a><li>
+\			<li><a href="http://project-four.jeremynixon.me/">Timestamp</a><li>
 			<li><a href="/home/contact">Contact</a><li>
 		</ul>
 </div>
