cc5c9fbfba3c1dabe82acf4ec0a1237ba3314417
jeremynixon <jnixon2@gmail.com>
2015-07-14 21:01:40 -0400

    add growth post

--- app/views/blog/post7.blade.php
+++ app/views/blog/post7.blade.php
@@ -1,6 +1,18 @@
 @extends('/blog/blog-base')
 @section('body')
 
-<h2>Logistic Regression or Linear Methods for Classificaiton</h2>
-
+<h2 class="growth">Growth as a Way of Life</h2>
+<p>
+Reference Dependence kills everything.
+</p><p>
+It does not discriminate between happiness and sadness. Your joy at having won the lottery? Gone after a few short months. Your pain at having lost a leg, lost a job? Gone after a few short months. Won a championship? Glory flashes, but then back to the grind. We adjust to change. And so achievement becomes hollow as soon as our expectation shifts to be the level that we’re at. Meeting that expectation becomes the new standard. 
+</p><p>
+I wanted to escape. Meeting standard wasn’t good enough, and standard is shifty. And so instead of placing the weight determining my happiness on results, I turned to the first derivative: growth.
+</p><p>
+Growth has a reference point too. But the beautiful thing is that that reference point barely adjusts. It’s adjustment is slow, and year after year of bliss in exceeding that expectation can follow. 
+</p><p>
+The beautiful thing is that a focus shift to growth turns the day to day object level behavior that drives success into the goal, in and of itself. You can ask yourself about growth on a weekly or even daily basis. The feedback loop shrinks dramatically, and so you hijack the reward cycle that drives so much passion seeking behavior.
+</p><p>
+And so there is escape. Escape from f(x) to the higher derivatives. To places that reference dependence lags to go.
+</p>
 @stop
