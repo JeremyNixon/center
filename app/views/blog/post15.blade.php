27e2dd5b559b9e18718a7581a2441690e7c29948
jeremynixon <jnixon2@gmail.com>
2015-07-21 21:45:36 -0400

    ridge regression input change

--- app/views/blog/post15.blade.php
+++ app/views/blog/post15.blade.php
@@ -19,22 +19,22 @@ $$ \hat{\beta}_{\lambda}^{ridge} = (X^TX + \lambda I_p)^{-1}X^Ty $$
 <p>
 Let's instantiate this equation in python code. lam is our lambda parameter. 
 </p>
-<!-- HTML generated using hilite.me --><div style="background: #272822; overflow:auto;width:auto;border-width:.1em .1em .1em .8em;padding:.2em .6em;"><pre style="margin: 0; line-height: 125%"><span style="color: #f92672">from</span> <span style="color: #f8f8f2">numpy</span> <span style="color: #f92672">import</span> <span style="color: #f8f8f2">array,</span> <span style="color: #f8f8f2">dot,</span> <span style="color: #f8f8f2">transpose</span>
-<span style="color: #f92672">from</span> <span style="color: #f8f8f2">numpy.linalg</span> <span style="color: #f92672">import</span> <span style="color: #f8f8f2">inv</span>
-<span style="color: #f92672">import</span> <span style="color: #f8f8f2">numpy</span> <span style="color: #f92672">as</span> <span style="color: #f8f8f2">np</span>
+<!-- HTML generated using hilite.me --><div style="background: #272822; overflow:auto;width:auto;border-width:.1em .1em .1em .8em;padding:.2em .6em;"><pre style="margin: 0; line-height: 125%"><span style="color: #f92672">import</span> <span style="color: #f8f8f2">numpy</span> <span style="color: #f92672">as</span> <span style="color: #f8f8f2">np</span>
 
-<span style="color: #66d9ef">def</span> <span style="color: #a6e22e">ridge_regression</span><span style="color: #f8f8f2">(points,</span> <span style="color: #f8f8f2">lam):</span>
+<span style="color: #66d9ef">def</span> <span style="color: #a6e22e">ridge_regression_update</span><span style="color: #f8f8f2">(x_train,</span> <span style="color: #f8f8f2">y_train,</span> <span style="color: #f8f8f2">lam):</span>
     
-    <span style="color: #f8f8f2">X</span> <span style="color: #f92672">=</span> <span style="color: #f8f8f2">array(points)</span>
-    <span style="color: #f8f8f2">X</span> <span style="color: #f92672">=</span> <span style="color: #f8f8f2">array([[</span><span style="color: #ae81ff">1</span><span style="color: #f8f8f2">]</span> <span style="color: #f92672">+</span> <span style="color: #f8f8f2">list(p[:</span><span style="color: #f92672">-</span><span style="color: #ae81ff">1</span><span style="color: #f8f8f2">])</span> <span style="color: #66d9ef">for</span> <span style="color: #f8f8f2">p</span> <span style="color: #f92672">in</span> <span style="color: #f8f8f2">X])</span>
-    <span style="color: #f8f8f2">y</span> <span style="color: #f92672">=</span> <span style="color: #f8f8f2">array([p[</span><span style="color: #f92672">-</span><span style="color: #ae81ff">1</span><span style="color: #f8f8f2">]</span> <span style="color: #66d9ef">for</span> <span style="color: #f8f8f2">p</span> <span style="color: #f92672">in</span> <span style="color: #f8f8f2">X])</span>
+    <span style="color: #f8f8f2">X</span> <span style="color: #f92672">=</span> <span style="color: #f8f8f2">np</span><span style="color: #f92672">.</span><span style="color: #f8f8f2">array(x_train)</span>
+    <span style="color: #f8f8f2">ones</span> <span style="color: #f92672">=</span> <span style="color: #f8f8f2">np</span><span style="color: #f92672">.</span><span style="color: #f8f8f2">ones(len(X))</span>
+    <span style="color: #f8f8f2">X</span> <span style="color: #f92672">=</span> <span style="color: #f8f8f2">np</span><span style="color: #f92672">.</span><span style="color: #f8f8f2">column_stack((ones,X))</span>
+    <span style="color: #f8f8f2">y</span> <span style="color: #f92672">=</span> <span style="color: #f8f8f2">np</span><span style="color: #f92672">.</span><span style="color: #f8f8f2">array(y_train)</span>
     
-    <span style="color: #f8f8f2">Xt</span> <span style="color: #f92672">=</span> <span style="color: #f8f8f2">transpose(X)</span>
+    <span style="color: #f8f8f2">Xt</span> <span style="color: #f92672">=</span> <span style="color: #f8f8f2">np</span><span style="color: #f92672">.</span><span style="color: #f8f8f2">transpose(X)</span>
     <span style="color: #f8f8f2">lambda_identity</span> <span style="color: #f92672">=</span> <span style="color: #f8f8f2">lam</span><span style="color: #f92672">*</span><span style="color: #f8f8f2">np</span><span style="color: #f92672">.</span><span style="color: #f8f8f2">identity(len(Xt))</span>
-    <span style="color: #f8f8f2">theInverse</span> <span style="color: #f92672">=</span> <span style="color: #f8f8f2">inv(dot(Xt,</span> <span style="color: #f8f8f2">X)</span><span style="color: #f92672">+</span><span style="color: #f8f8f2">lambda_identity)</span>
-    <span style="color: #f8f8f2">w</span> <span style="color: #f92672">=</span> <span style="color: #f8f8f2">dot(dot(theInverse,</span> <span style="color: #f8f8f2">Xt),</span> <span style="color: #f8f8f2">y)</span>
+    <span style="color: #f8f8f2">theInverse</span> <span style="color: #f92672">=</span> <span style="color: #f8f8f2">np</span><span style="color: #f92672">.</span><span style="color: #f8f8f2">linalg</span><span style="color: #f92672">.</span><span style="color: #f8f8f2">inv(np</span><span style="color: #f92672">.</span><span style="color: #f8f8f2">dot(Xt,</span> <span style="color: #f8f8f2">X)</span><span style="color: #f92672">+</span><span style="color: #f8f8f2">lambda_identity)</span>
+    <span style="color: #f8f8f2">w</span> <span style="color: #f92672">=</span> <span style="color: #f8f8f2">np</span><span style="color: #f92672">.</span><span style="color: #f8f8f2">dot(np</span><span style="color: #f92672">.</span><span style="color: #f8f8f2">dot(theInverse,</span> <span style="color: #f8f8f2">Xt),</span> <span style="color: #f8f8f2">y)</span>
     <span style="color: #66d9ef">return</span> <span style="color: #f8f8f2">w,</span> <span style="color: #66d9ef">lambda</span> <span style="color: #f8f8f2">x:</span> <span style="color: #f8f8f2">dot(w,x)</span>
 </pre></div>
+
 
 
 @stop
